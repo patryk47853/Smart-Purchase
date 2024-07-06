2b3c7dcaefaff9644449ce2b4af9aa288e5147fd
Patryk <109095669+patryk47853@users.noreply.github.com>
2024-07-06 16:15:49 +0200

    Implement and handle exceptions

---
+++ services/customer/src/main/java/pl/patrykjava/ecommerce/exception/CustomerNotFoundException.java
@@ -0,0 +1,10 @@
+package pl.patrykjava.ecommerce.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CustomerNotFoundException extends RuntimeException {
+    private final String msg;
+}
