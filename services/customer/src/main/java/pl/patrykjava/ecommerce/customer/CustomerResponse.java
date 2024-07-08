417f48cd2b292231c7a422cd3f490650b0f13c09
Patryk <109095669+patryk47853@users.noreply.github.com>
2024-07-08 13:09:32 +0200

    Implement Response for Customer layer

---
+++ services/customer/src/main/java/pl/patrykjava/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,10 @@
+package pl.patrykjava.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstName,
+        String lastName,
+        String email,
+        Address address
+) {
+}
