a1dc419f1c423ef00f26c2fb27ee3323becab327
Patryk <109095669+patryk47853@users.noreply.github.com>
2024-07-09 17:35:10 +0200

    Implement Exceptions and their handling for Product layer

---
+++ services/product/src/main/java/pl/patrykjava/ecommerce/exception/ErrorResponse.java
@@ -0,0 +1,8 @@
+package pl.patrykjava.ecommerce.exception;
+
+import java.util.Map;
+
+public record ErrorResponse(
+            Map<String, String> errors
+) {
+}
---
+++ services/product/src/main/java/pl/patrykjava/ecommerce/exception/ProductPurchaseException.java
@@ -0,0 +1,8 @@
+package pl.patrykjava.ecommerce.exception;
+
+public class ProductPurchaseException extends RuntimeException {
+    public ProductPurchaseException(String message) {
+        super(message);
+    }
+}
+
