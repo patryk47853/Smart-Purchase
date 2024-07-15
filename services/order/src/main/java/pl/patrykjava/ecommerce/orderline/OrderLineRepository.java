5f4a5f3d4d8fc269850f9e81cf140ba32b6d491d
Patryk <109095669+patryk47853@users.noreply.github.com>
2024-07-15 11:07:10 +0200

    Implement Order Line repository for Order layer

---
+++ services/order/src/main/java/pl/patrykjava/ecommerce/orderline/OrderLineRepository.java
@@ -0,0 +1,8 @@
+package pl.patrykjava.ecommerce.orderline;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
+    List<OrderLine> findAllByOrderId(Long orderId);
+}
