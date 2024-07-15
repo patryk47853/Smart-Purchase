23bf24eba0bf41e86ad299b2e51d5ffc2ecca2da
Patryk <109095669+patryk47853@users.noreply.github.com>
2024-07-15 11:06:55 +0200

    Implement Order Line mapper for Order layer

---
+++ services/order/src/main/java/pl/patrykjava/ecommerce/orderline/OrderLineMapper.java
@@ -0,0 +1,28 @@
+package pl.patrykjava.ecommerce.orderline;
+
+import org.springframework.stereotype.Service;
+import pl.patrykjava.ecommerce.order.Order;
+
+@Service
+public class OrderLineMapper {
+    public OrderLine toOrderLine(OrderLineRequest request) {
+        return OrderLine.builder()
+                .id(request.id())
+                .quantity(request.quantity())
+                .order(
+                        Order.builder()
+                                .id(request.orderId())
+                                .build()
+                )
+                .productId(request.productId())
+
+                .build();
+    }
+
+    public OrderLineResponse toOrderLineResponse(OrderLine orderLine) {
+        return new OrderLineResponse(
+                orderLine.getId(),
+                orderLine.getQuantity()
+        );
+    }
+}
